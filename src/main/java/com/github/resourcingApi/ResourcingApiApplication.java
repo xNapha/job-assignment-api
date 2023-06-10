bd2044a40e1562989a57b5baf44e61b7035192d2
xNapha <nathancai1997@gmail.com>
2023-06-10 15:28:50 +1000

    initial commit

---
+++ src/main/java/com/github/resourcingApi/ResourcingApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.resourcingApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/resourcingApi/ResourcingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.resourcingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
